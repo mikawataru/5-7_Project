560a7f0b8ed7271ccde17facc55bd7bae3812501
mikawataru <ahonokonoha@gmail.com>
2018-01-03 00:46:22 +0900

    first commit

---
+++ Assets/Script/DBtest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DBtest : MonoBehaviour {
+    // Use this for initialization
+    DB_Client DB = new DB_Client();
+    const int CHARA_ID = 1;
+    int Frame_id = 0;
+
+    void Start () {
+        
+    }
+        
+    // Update is called once per frame
+    void Update () {
+	}
+}
