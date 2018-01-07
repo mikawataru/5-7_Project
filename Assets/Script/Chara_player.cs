286000144600c02f128b483cda6d574c1ed6354f
mikawataru <ahonokonoha@gmail.com>
2018-01-08 07:45:49 +0900

    add chara motion

---
+++ Assets/Script/Chara_player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Chara_player : MonoBehaviour
+{
+    public Camera Camera;
+    GameObject Model;
+    Animator Anim;
+    enum MOVE : int { I = 0, F = 1, B = 2, R = 3, L = 4, FF = 5};
+    MOVE move;
+
+    public float Speed = 4f;
+    public float Speed_fast = 4f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Model = GameObject.FindGameObjectWithTag("Chara_model");
+        Anim = Model.GetComponent<Animator>();
+        move = MOVE.I;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("GamePad1_Horizontal");
+        float y = Input.GetAxis("GamePad1_Vertical");
+        float speed = Speed;
+                
+        if (x * x + y * y > 0.25)
+        {
+            float dir = Mathf.Atan(y / x) * Mathf.Rad2Deg;
+
+            if (Mathf.Abs(dir) >= 45)
+            {
+                if (y > 0)
+                {
+                    if(y > 0.6)
+                    {
+                        speed = Speed_fast;
+                        Anim.SetInteger("Move", (int)MOVE.FF);
+                    }
+                    else
+                    {
+                        Anim.SetInteger("Move", (int)MOVE.F);
+                    }
+                }
+                else
+                {
+                    Anim.SetInteger("Move", (int)MOVE.B);
+                }
+            }
+            else
+            {
+                if (x > 0)
+                {
+                    Anim.SetInteger("Move", (int)MOVE.R);
+                }
+                else
+                {
+                    Anim.SetInteger("Move", (int)MOVE.L);
+                }
+            }
+
+        }
+        else
+        {
+            Anim.SetInteger("Move", 0);
+        }
+
+        transform.position += transform.forward * speed * y * Time.deltaTime;
+        transform.position += transform.right * Speed * x * Time.deltaTime;
+    }
+}
---
+++ Assets/Script/gamepad_test.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class gamepad_test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    
+	void Update () {
+        float h = Input.GetAxis("GamePad1_Horizontal");
+        float v = Input.GetAxis("GamePad1_Vertical");
+        float x = Input.GetAxis("GamePad1_Cross_X");
+        float y = Input.GetAxis("GamePad1_Cross_Y");
+        if ( h*h+v*v > 0.25 ) Debug.Log("h:" + h + ",v:" + v);
+        if ( x*x+y*y > 0.25) Debug.Log("x:" + x + ",y:" + y);
+        if (Input.GetButton("GamePad1_A")) Debug.Log("A");
+        if (Input.GetButton("GamePad1_B")) Debug.Log("B");
+        if (Input.GetButton("GamePad1_X")) Debug.Log("X");
+        if (Input.GetButton("GamePad1_Y")) Debug.Log("Y");
+        if (Input.GetButton("GamePad1_R1")) Debug.Log("R1");
+        if (Input.GetButton("GamePad1_L1")) Debug.Log("L1");
+        if (Input.GetButton("GamePad1_R2")) Debug.Log("R2");
+        if (Input.GetButton("GamePad1_L2")) Debug.Log("L2");
+        if (Input.GetButton("GamePad1_Start")) Debug.Log("Start");
+        if (Input.GetButton("GamePad1_Select")) Debug.Log("Select");
+    }
+
+    /*
+    void Update()
+    {
+        DownKeyCheck();
+    }
+
+
+    void DownKeyCheck()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    //処理を書く
+                    Debug.Log(code);
+                    break;
+                }
+            }
+        }
+    }
+    */
+}
